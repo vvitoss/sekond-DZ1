274768d5fb36f7ea4becab2149d62d157c3f8531
vvitoss <orangekaramel@gmail.com>
2021-12-29 10:04:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int x = 20;
+        int bonus = price/x;
+        System.out.println(bonus + " мили");
+    }
+}
